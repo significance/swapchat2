5b6782a9b08bf5431284cc64859f411dd833c4a7
sig <dan@1up.digital>
2021-12-28 13:16:16 +0000

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useLocation } from "react-router-dom";
+
+import Chat from "./components/Chat";
+
+const apiURL = "http://localhost:1633";
+const debugURL = "http://localhost:1635";
 
 function App() {
+  const search = useLocation().search;
+  const token = new URLSearchParams(search).get("token");
+  const chatRole = token && token.length === 194 ? "respondent" : "initiator";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">SwapChat</header>
+      <Chat
+        chatRole={chatRole}
+        token={token}
+        apiURL={apiURL}
+        debugURL={debugURL}
+      />
     </div>
   );
 }
